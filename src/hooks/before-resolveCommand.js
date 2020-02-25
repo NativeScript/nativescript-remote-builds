c96b2989e48171b11b25b848dcc7d30532c4fd02
DimitarTachev <nevermindd7@gmail.com>
2020-02-25 15:25:33 +0200

    fix: skip iosProjectService validation for remote builds

--- src/hooks/before-resolveCommand.js
+++ src/hooks/before-resolveCommand.js
@@ -1,11 +1,12 @@
 
-module.exports = (hookArgs, $platformValidationService) => {
+module.exports = (hookArgs, $platformValidationService, $iOSProjectService) => {
     if (hookArgs.argv.indexOf("--env.local") > -1) {
         // let the local publish
         return;
     }
 
     $platformValidationService.isPlatformSupportedForOS = () => true;
+    $iOSProjectService.validate = () => ({ checkEnvironmentRequirementsOutput: { canExecute: true } });
 
     return (args) => {
         // commandName: string, commandArguments: string[], argv: string[]

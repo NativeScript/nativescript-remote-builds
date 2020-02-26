46785ab5dd69a94f134cd33193e0bf6d1d1e34c3
DimitarTachev <nevermindd7@gmail.com>
2020-02-26 10:57:48 +0200

    fix: do not validate android sdk for cloud builds

--- src/hooks/before-resolveCommand.js
+++ src/hooks/before-resolveCommand.js
@@ -1,5 +1,5 @@
 
-module.exports = (hookArgs, $platformValidationService, $iOSProjectService) => {
+module.exports = (hookArgs, $platformValidationService, $iOSProjectService, $androidToolsInfo) => {
     if (hookArgs.argv.indexOf("--env.local") > -1) {
         // let the local publish
         return;
@@ -7,6 +7,7 @@ module.exports = (hookArgs, $platformValidationService, $iOSProjectService) => {
 
     $platformValidationService.isPlatformSupportedForOS = () => true;
     $iOSProjectService.validate = () => ({ checkEnvironmentRequirementsOutput: { canExecute: true } });
+    $androidToolsInfo.validateTargetSdk = () => false;
 
     return (args) => {
         // commandName: string, commandArguments: string[], argv: string[]
